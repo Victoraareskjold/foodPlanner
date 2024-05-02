904a80776ace505d4b7f202871cfe49309f420c5
Victor Aareskjold <victor.aaareskjold@gmail.com>
2024-05-02 21:41:49 +0200

    shopping ingredients now stack and work with weekmenu

--- src/screens/ShoppingList.js
+++ src/screens/ShoppingList.js
@@ -5,6 +5,8 @@ import {
   ScrollView,
   StyleSheet,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  SafeAreaView,
 } from "react-native";
 import { db, auth } from "../../firebase";
 import {
@@ -15,6 +17,7 @@ import {
   doc,
   getDoc,
 } from "firebase/firestore";
+import containerStyles from "../../styles/containerStyles";
 
 const ShoppingList = () => {
   const [ingredients, setIngredients] = useState([]);
@@ -26,19 +29,45 @@ const ShoppingList = () => {
         const userDocSnap = await getDoc(userDocRef);
         if (userDocSnap.exists()) {
           const familyId = userDocSnap.data().familyId;
-          const q = query(
+          const weekMenuRef = collection(db, "families", familyId, "weekMenu");
+          const weekMenuSnap = await getDocs(weekMenuRef);
+          let recipeCounts = {}; // Objekt for å telle antall forekomster av hver recipeId
+          weekMenuSnap.forEach((doc) => {
+            const recipeId = doc.data().recipeId;
+            recipeCounts[recipeId] = (recipeCounts[recipeId] || 0) + 1; // Inkrementer tellingen for hver recipeId
+          });
+
+          const recipesQuery = query(
             collection(db, "recipes"),
             where("familyId", "==", familyId)
           );
-          const querySnapshot = await getDocs(q);
-          let allIngredients = [];
-
-          querySnapshot.forEach((doc) => {
-            const recipeIngredients = doc.data().ingredients || [];
-            allIngredients.push(...recipeIngredients);
+          const recipesSnapshot = await getDocs(recipesQuery);
+          let ingredientMap = {}; // For å aggregere ingredienser
+          recipesSnapshot.forEach((doc) => {
+            const count = recipeCounts[doc.id] || 0;
+            if (count > 0) {
+              const recipeIngredients = doc.data().ingredients || [];
+              recipeIngredients.forEach((ingredient) => {
+                if (ingredient.name && ingredient.name.length > 1) {
+                  const key = `${ingredient.name.toLowerCase()}|${
+                    ingredient.unit
+                  }`;
+                  const quantityToAdd = parseFloat(ingredient.quantity) * count;
+                  if (ingredientMap[key]) {
+                    ingredientMap[key].quantity += quantityToAdd;
+                  } else {
+                    ingredientMap[key] = {
+                      ...ingredient,
+                      quantity: quantityToAdd,
+                    };
+                  }
+                }
+              });
+            }
           });
 
-          setIngredients(allIngredients);
+          const aggregatedIngredients = Object.values(ingredientMap);
+          setIngredients(aggregatedIngredients);
         } else {
           console.log("No user data found");
         }
@@ -49,19 +78,31 @@ const ShoppingList = () => {
   }, []);
 
   return (
-    <ScrollView style={styles.container}>
-      {ingredients.map((ingredient, index) => (
-        <View key={index} style={styles.ingredientContainer}>
-          <TouchableOpacity>
-            <Text>
-              {ingredient.quantity}
-              {ingredient.unit}
-              {ingredient.name}
-            </Text>
-          </TouchableOpacity>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={{ flex: 1 }}
+      keyboardVerticalOffset={Platform.OS === "ios" ? 90 : 0}
+    >
+      <ScrollView style={styles.container}>
+        <SafeAreaView />
+        <View style={[containerStyles.defaultContainer, { gap: 32 }]}>
+          <View style={styles.list}>
+            {ingredients.map((ingredient, index) => (
+              <View key={index} style={styles.listItems}>
+                <TouchableOpacity style={styles.listItem}>
+                  <View style={styles.listText}>
+                    <Text>{ingredient.quantity}</Text>
+                    <Text>{ingredient.unit}</Text>
+                    <Text style={{ marginStart: 8 }}>{ingredient.name}</Text>
+                  </View>
+                  <View style={styles.checkBox}></View>
+                </TouchableOpacity>
+              </View>
+            ))}
+          </View>
         </View>
-      ))}
-    </ScrollView>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -70,12 +111,29 @@ export default ShoppingList;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    padding: 10,
+    backgroundColor: "#fff",
   },
-  ingredientContainer: {
-    marginBottom: 10,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
-    paddingBottom: 5,
+  list: {
+    gap: 12,
+  },
+  listItems: {
+    paddingVertical: 8,
+    paddingHorizontal: 6,
+    borderRadius: 5,
+  },
+  listItem: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  listText: {
+    flexDirection: "row",
+    gap: 4,
+  },
+  checkBox: {
+    height: 16,
+    width: 16,
+    borderColor: "blue",
+    borderWidth: 1,
+    borderRadius: 50,
   },
 });
